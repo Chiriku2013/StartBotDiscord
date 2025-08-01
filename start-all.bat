d3af16d25f81c7b1e73d2fa66127d2c87dae4cae
Chiriku2013 <suzukichiriku@gmail.com>
2025-08-01 12:46:58 +0700

    Update start-all.bat

--- start-all.bat
+++ start-all.bat
@@ -1,8 +1,11 @@
 @echo off
-title Start All Discord Bots
+echo [STARTING] All bots...
 
 start cmd /k "cd /d C:\Desktop\Bot Discord\Brainrot Quiz && npm start"
 start cmd /k "cd /d C:\Desktop\Bot Discord\ChatGPT Smart && npm start"
 start cmd /k "cd /d C:\Desktop\Bot Discord\Meme Vietnamese && npm start"
 start cmd /k "cd /d C:\Desktop\Bot Discord\Chiriku Casino && npm start"
 start cmd /k "cd /d C:\Desktop\Bot Discord\Chiriku Giveaway && npm start"
+
+echo [DONE] All bots started!
+pause
